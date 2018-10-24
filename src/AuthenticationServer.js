e52915387bf08dbf2fff90e74e9f1763e9ea95e9
Jeremy <jsc126@case.edu>
2018-10-23 20:52:10 -0400

    init repo

---
+++ src/AuthenticationServer.js
@@ -0,0 +1,3 @@
+//Jeremy Chan + Chris Zhang
+//393 Final Project
+//Node.js server that handles all the Authentication related tasks
---
+++ src/OperationalServer.js
@@ -0,0 +1,3 @@
+//Jeremy Chan + Chris Zhang
+//393 Final Project
+//Node.js server that handles backend tasks for Room.io app
