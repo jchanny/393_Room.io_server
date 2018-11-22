4882dda9b72e68ae515f33474cc1a33124790d7f
jeremy <jsc126@case.edu>
2018-11-22 00:26:38 -0500

    did something

--- src/OperationalServer/OperationalServer.js
+++ src/OperationalServer/OperationalServer.js
@@ -8,7 +8,7 @@ const connStr = 'mongodb://localhost:27017';
 
 //returns true if user_id, password match a user in Auth Database
 async function checkCredentials(user_id, password){
-    if(typeof user_id != 'number' || typeof password != 'string')
+    if(typeof user_id != 'string' || typeof password != 'string')
 	return 'Input argument is of wrong type';
     
    return await AuthServer.validateCredentials(user_id, password, function(res){
@@ -25,7 +25,7 @@ async function checkCredentials(user_id, password){
 
 //adds new user to the Auth Database and creates a new user JSON field
 async function createNewUser(user_id, password, email, name, group_id){
-    if(typeof user_id != 'number' || typeof password != 'string' || typeof group_id != 'number')
+    if(typeof user_id != 'string' || typeof password != 'string' || typeof group_id != 'string')
 	return 'Input argument is of wrong type';
 
     var doesUserAlreadyExist = await AuthServer.checkIfUserExists(user_id);
@@ -43,79 +43,79 @@ async function createNewUser(user_id, password, email, name, group_id){
     }
 }
 
-async function fetchGroupData(group_id){
-    if(typeof group_id != 'number')
-	return 'Input argument is of wrong type.';
+// async function fetchGroupData(group_id){
+//     if(typeof group_id != 'string')
+// 	return 'Input argument is of wrong type.';
     
-    var result = await MongoClient.connect(connStr, function(err, client){
-	if(err) throw err;
+//     var result = await MongoClient.connect(connStr, function(err, client){
+// 	if(err) throw err;
 	
-	const db = client.db('test');
+// 	const db = client.db('test');
 	
-	db.collection("OperationalDatabase").find({"group_id" : group_id}, function(error, result){
-	    if(error) throw error;
-	    client.close();
+// 	db.collection("OperationalDatabase").find({"group_id" : group_id}, function(error, result){
+// 	    if(error) throw error;
+// 	    client.close();
 	    
-	    return result;
-	});
-    });
+// 	    return result;
+// 	});
+//     });
 
-    return result;
-}
+//     return result;
+// }
 
 
-//returns user data json field 
-async function getUserData(user_id){
-    if(typeof user_id != 'number')
-	return 'Input argument is of wrong type';
+// //returns user data json field 
+// async function getUserData(user_id){
+//     if(typeof user_id != 'string')
+// 	return 'Input argument is of wrong type';
 
-    var group = await AuthServer.getUserGroup(user_id, function(results){
-	return results;
-    });
+//     var group = await AuthServer.getUserGroup(user_id, function(results){
+// 	return results;
+//     });
 
-    if(group == 'User does not exist'){
-	return 'User does not exist';
-    }
+//     if(group == 'User does not exist'){
+// 	return 'User does not exist';
+//     }
 
-    var groupData = await fetchGroupData(group);
+//     var groupData = await fetchGroupData(group);
     
-    var memberDataList = groupData[members];
-    return memberDataList.find(x => x["user_id"] === user_id);
+//     var memberDataList = groupData[members];
+//     return memberDataList.find(x => x["user_id"] === user_id);
     
-}
+// }
 
-async function createGroup(group_owner, group_id, group_name){
-    if(typeof group_owner != 'number' || typeof group_id != 'number' || typeof group_name != 'string')
-	return 'Input argument is of wrong type.';
+// async function createGroup(group_owner, group_id, group_name){
+//     if(typeof group_owner != 'string' || typeof group_id != 'string' || typeof group_name != 'string')
+// 	return 'Input argument is of wrong type.';
     
-    var group_owner_json = {
-	"user_id" : group_owner,
-	"member_group_id" : group_id,
-	"admined_group_id" : group_id,
-	"involved_tabs" : [],
-	"notifications" : []
-    };
+//     var group_owner_json = {
+// 	"user_id" : group_owner,
+// 	"member_group_id" : group_id,
+// 	"admined_group_id" : group_id,
+// 	"involved_tabs" : [],
+// 	"notifications" : []
+//     };
     
-    var document = {
-	"group_id" : group_id,
-	"group_name" : group_name,
-	"group_admin_user_id" : group_owner,
-	"members" : [group_owner_json],
-	"group_tasks" : [],
-	"messages" : []
-    };
+//     var document = {
+// 	"group_id" : group_id,
+// 	"group_name" : group_name,
+// 	"group_admin_user_id" : group_owner,
+// 	"members" : [group_owner_json],
+// 	"group_tasks" : [],
+// 	"messages" : []
+//     };
     
-   return MongoClient.connect(connStr, function(err, client){
-	if(err) throw err;
+//    return MongoClient.connect(connStr, function(err, client){
+// 	if(err) throw err;
 	
-	const db = client.db('test');
+// 	const db = client.db('test');
 	
-	db.collection("OperationalDatabase").insert(document, function(error, result){
-	    if(error) throw error;
-	    client.close();
-	});
-    });
-}
+// 	db.collection("OperationalDatabase").insert(document, function(error, result){
+// 	    if(error) throw error;
+// 	    client.close();
+// 	});
+//     });
+// }
 
 
 
--- test/OperationalServerTest.js
+++ test/OperationalServerTest.js
@@ -2,21 +2,21 @@ const assert = require('assert');
 const OpServer = require('../src/OperationalServer/OperationalServer.js');
 
 describe('test checkCredentials()' ,function(){
-    it("Should return error if user_id not int", function(){
-	OpServer.checkCredentials("11111", "aaaas", function(result){
+    it("Should return error if user_id not string", function(){
+	OpServer.checkCredentials(11111, "aaaas", function(result){
 	    assert.equal(result, 'Input argument is of wrong type.');
 	});
     });
 
     it("Should return error if password not string", function(){
-	OpServer.checkCredentials(1111, 11, function(result){
+	OpServer.checkCredentials("1111", 11, function(result){
 	    assert.equal(result, 'Input argument is of wrong type.');
 	});
     });
 
     it("Should return true if credentials are valid", async function(){
 	try{
-	    var result = await OpServer.checkCredentials(111, "a");
+	    var result = await OpServer.checkCredentials("111", "a");
 	    return assert.equal(result, true);
 	}
 	catch(err){
@@ -27,7 +27,7 @@ describe('test checkCredentials()' ,function(){
 
     it("Should return false if credentials are not valid", async function(){
 	try{
-	    var result = await OpServer.checkCredentials(111, "b");
+	    var result = await OpServer.checkCredentials("111", "b");
 	    return assert.equal(result, false);
 	}
 	catch(err){
@@ -39,27 +39,27 @@ describe('test checkCredentials()' ,function(){
 });
 
 describe('test createNewUser()', function(){
-    it('should throw error if user_id not num', function(){
-	OpServer.createNewUser("111", "a", 222, function(result){
-	    assert.equal(result, 'Input argument is of wrong type.');
-	});
-    });
-
-    it('should throw error if group_id not num', function(){
+    it('should throw error if user_id not string', function(){
 	OpServer.createNewUser(111, "a", "222", function(result){
 	    assert.equal(result, 'Input argument is of wrong type.');
 	});
     });
 
+    it('should throw error if group_id not num', function(){
+	OpServer.createNewUser("111", "a", 222, function(result){
+	    assert.equal(result, 'Input argument is of wrong type.');
+	});
+    });
+
     it('should throw error if password not string', function(){
-	OpServer.createNewUser(111, 22, 222, function(result){
+	OpServer.createNewUser("111", "22", 222, function(result){
 	    assert.equal(result, 'Input argument is of wrong type.');
 	});
     });
 
     it('Should return error if username already exists', async function(){
 	try{
-	    var result = await OpServer.createNewUser(111, "fdsd", "test.test", "f", 2222);
+	    var result = await OpServer.createNewUser("111", "fdsd", "test.test", "f", 2222);
 	    return assert.equal(result, 'User already exists, please select another username.');
 	}
 	catch(err){
@@ -71,78 +71,78 @@ describe('test createNewUser()', function(){
     
 });
 
-describe('test fetchGroupData()', function(){
-    it('should throw error if type of user_id not int', function(){
-	var result = OpServer.getUserData("111");
-	assert.equal(result, 'Input argument is of wrong type.');
-    });
+// describe('test fetchGroupData()', function(){
+//     it('should throw error if type of user_id not int', function(){
+// 	var result = OpServer.getUserData("111");
+// 	assert.equal(result, 'Input argument is of wrong type.');
+//     });
 
-    it('Should return group does not exist if group not found',async function(){
-	try{
-	    assert.fail();
-	}catch(err){
-	    console.log(err);
-	    assert.fail();
-	}
-    });
-});
+//     it('Should return group does not exist if group not found',async function(){
+// 	try{
+// 	    assert.fail();
+// 	}catch(err){
+// 	    console.log(err);
+// 	    assert.fail();
+// 	}
+//     });
+// });
 
-describe('test getUserData()', function(){
-    it('should throw error if type of user_id not int', function(){
-	var result = OpServer.getUserData("1111");
-	assert.equal(result, 'Input argument is of wrong type.');
-    });
+// describe('test getUserData()', function(){
+//     it('should throw error if type of user_id not int', function(){
+// 	var result = OpServer.getUserData("1111");
+// 	assert.equal(result, 'Input argument is of wrong type.');
+//     });
 
-    it('should return user does not exist', async function(){
-	try{
-	    var results = await OpServer.getUserData(9999);
-	    return assert.equal(results, 'User does not exist');
-	}catch(err){
-	    console.log(err);
-	    return assert.fail();
-	}
-    });
+//     it('should return user does not exist', async function(){
+// 	try{
+// 	    var results = await OpServer.getUserData(9999);
+// 	    return assert.equal(results, 'User does not exist');
+// 	}catch(err){
+// 	    console.log(err);
+// 	    return assert.fail();
+// 	}
+//     });
 
-    it('should return the user data', async function(){
-	try{
-	    var results = await OpServer.getUserData(666);
-	    return assert.equal(results, 
-		{ "user_id" : 666, "member_group_id" : 666, "admined_group_id" : 666, "involved_tabs" : [ ], "notifications" : [ ] } 
-	    );
-	}
-	catch(err){
-	    console.log(err);
-	    return assert.fail();
-	}
-    })
+//     it('should return the user data', async function(){
+// 	try{
+// 	    var results = await OpServer.getUserData(666);
+// 	    return assert.equal(results, 
+// 		{ "user_id" : 666, "member_group_id" : 666, "admined_group_id" : 666, "involved_tabs" : [ ], "notifications" : [ ] } 
+// 	    );
+// 	}
+// 	catch(err){
+// 	    console.log(err);
+// 	    return assert.fail();
+// 	}
+//     })
 
-});
+// });
 
-describe('test createGroup()', function(){
-    it('should throw error if group_owner not num', function(result){
-	var result = OpServer.createGroup("f", 1112, "test");
-	assert.equal(result, 'Input argument is of wrong type.');
-    });
+// describe('test createGroup()', function(){
+//     it('should throw error if group_owner not num', function(result){
+// 	var result = OpServer.createGroup("f", 1112, "test");
+// 	assert.equal(result, 'Input argument is of wrong type.');
+//     });
 
-    it('should throw error if group_id not num', function(result){
-	var result = OpServer.createGroup(1112, '1112', "test");
-	assert.equal(result, 'Input argument is of wrong type.');
-    });
+//     it('should throw error if group_id not num', function(result){
+// 	var result = OpServer.createGroup(1112, '1112', "test");
+// 	assert.equal(result, 'Input argument is of wrong type.');
+//     });
 
-    it('should throw error if group_name not string', function(result){
-	var result = OpServer.createGroup(1112, 1112, 1112);
-	assert.equal(result, 'Input argument is of wrong type.');
-    });
+//     it('should throw error if group_name not string', function(result){
+// 	var result = OpServer.createGroup(1112, 1112, 1112);
+// 	assert.equal(result, 'Input argument is of wrong type.');
+//     });
 
-    it('Should add correctly to database', async function(result){
-	try{
-	    await OpServer.createGroup(666, 666, "test");
-	    //need to check against MongoDB
-	}
-	catch(err){
-	    console.log(err);
-	    assert.fail();
-	}
-    });
+//     it('Should add correctly to database', async function(result){
+// 	try{
+// 	    await OpServer.createGroup(666, 666, "test");
+// 	    //need to check against MongoDB
+// 	}
+// 	catch(err){
+// 	    console.log(err);
+// 	    assert.fail();
+// 	}
+//     });
     
-});
+// });
